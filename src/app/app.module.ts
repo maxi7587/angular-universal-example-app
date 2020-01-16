b5c63df7d118e96f506a6d821983c9b1ee5ce987
Maximiliano Cruz <maxi7587@hotmail.com>
2020-01-16 15:28:04 -0300

    working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,8 @@ import { InMemoryDataService }  from './in-memory-data.service';
 import { AppRoutingModule }     from './app-routing.module';
 
 import { AppComponent }         from './app.component';
-import { DashboardComponent }   from './dashboard/dashboard.component';
-import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
-import { HeroesComponent }      from './heroes/heroes.component';
-import { HeroSearchComponent }  from './hero-search/hero-search.component';
 import { HeroService }          from './hero.service';
 import { MessageService }       from './message.service';
-import { MessagesComponent }    from './messages/messages.component';
 
 import { PLATFORM_ID, APP_ID, Inject } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
@@ -23,7 +18,7 @@ import { isPlatformBrowser } from '@angular/common';
 
 @NgModule({
   imports: [
-    BrowserModule.withServerTransition({ appId: 'tour-of-heroes' }),
+    BrowserModule.withServerTransition({ appId: 'saldo' }),
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
@@ -31,15 +26,7 @@ import { isPlatformBrowser } from '@angular/common';
       InMemoryDataService, { dataEncapsulation: false }
     )
   ],
-  declarations: [
-    AppComponent,
-    DashboardComponent,
-    HeroesComponent,
-    HeroDetailComponent,
-    MessagesComponent,
-    HeroSearchComponent
-  ],
-  providers: [ HeroService, MessageService ],
+  declarations: [ AppComponent ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule {
